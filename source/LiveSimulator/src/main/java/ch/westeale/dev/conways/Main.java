f16db7c74e6eecb69f15d1f5ecddac7da4ffab8a
westeale <westeale@students.zhaw.ch>
2017-10-18 17:51:07 +0200

    init project in eclipse

---
+++ source/LiveSimulator/src/main/java/ch/westeale/dev/conways/Main.java
@@ -0,0 +1,14 @@
+package ch.westeale.dev.conways;
+
+
+/*
+ * Main Class to start the Game
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.printf("Started");
+
+	}
+
+}
